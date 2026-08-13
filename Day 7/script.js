93552f0f82f1e492a556fe0559e84cf11f702dc3
Aman Kushwaha <ashayar630@gmail.com>
2026-08-13 19:07:50 +0530

    Day 7

---
+++ Day 7/script.js
@@ -0,0 +1,21 @@
+// if direct i make a variable but not insert any type of data then get value is undefine     and     also get the typeof undefine
+
+
+var a;
+console.log(a);
+console.log(typeof(a));
+var a=10;
+console.log(a);
+console.log(typeof(a));
+var a=22.22;
+console.log(a);
+console.log(typeof(a));
+var a=true;
+console.log(a);
+console.log(typeof(a));
+var a=["a","b","c",10,20,30];
+console.log(a);
+console.log(typeof(a));
+var a="BDC";
+console.log(a);
+console.log(typeof(a));
---
+++ Day 7/script_2.js
@@ -0,0 +1,5 @@
+console.log(a);
+console.log(typeof(a));
+var a=11;
+console.log(a);
+console.log(typeof(a));
